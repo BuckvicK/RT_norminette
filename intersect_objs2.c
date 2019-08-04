1d9cdc2ec642a1de633a3bb104cb54a4d50d78c6
Artem <artem@Artems-iMac.local>
2019-08-04 23:45:11 +0300

    Start norminette intersect

--- intersect_objs2.c
+++ intersect_objs2.c
@@ -1,41 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersect_objs.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chorange <chorange@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/12 10:41:13 by chorange          #+#    #+#             */
+/*   Updated: 2019/07/08 21:30:16 by chorange         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rtv1.h"
 
-double ray_intersect_paraboloid(t_vector start, t_vector dir, t_obj *parab)
-{
-	t_s		q;
-	t_vector 	x, hitpoint;
-	double dist ,u , z, len;
-	double zeroThreshold = 0.0001;
+// double ray_intersect_paraboloid(t_vector start, t_vector dir, t_obj *parab)
+// {
+// 	t_s		q;
+// 	t_vector 	x, hitpoint;
+// 	double dist ,u , z, len;
+// 	double zeroThreshold = 0.0001;
 
-	x = vector_subt(start, parab->center);
+// 	x = vector_subt(start, parab->center);
 
-	u = scal_mult(dir, parab->dir);
-	z = scal_mult(x, parab->dir);
+// 	u = scal_mult(dir, parab->dir);
+// 	z = scal_mult(x, parab->dir);
 
-	q.a = 2.f * (scal_mult(dir, dir) - u * u);
-	q.b = 2.f * (scal_mult(dir, x) - u * (z + 2.f *  parab->radius));
-	q.c = scal_mult(x, x) - z * (z + 4.f *  parab->radius);
-	if ((q.diskr = q.b * q.b - 2.f * q.a * q.c) >= 0.f)
-	{
-		q.sq_diskr = sqrt(q.diskr);
-		q.t = (-q.b - q.sq_diskr) / q.a;
-		if (q.t > 0.f) {
-			hitpoint = vector_sum(vector_int_mult(dir, q.t), x);
-			len = scal_mult(hitpoint, parab->dir);
-			if (len < parab->angle && len > -zeroThreshold)
-				return (q.t);
-		}
-		q.t = (-q.b + q.sq_diskr) / q.a;
-		if (q.t > -zeroThreshold) {
-			hitpoint = vector_sum(vector_int_mult(dir, q.t), x);
-			len = scal_mult(hitpoint, parab->dir);
-			if (len < parab->angle && len > -zeroThreshold)
-				return (q.t);
-		}
-	}
-	return (0);
+// 	q.a = 2.f * (scal_mult(dir, dir) - u * u);
+// 	q.b = 2.f * (scal_mult(dir, x) - u * (z + 2.f *  parab->radius));
+// 	q.c = scal_mult(x, x) - z * (z + 4.f *  parab->radius);
+// 	if ((q.diskr = q.b * q.b - 2.f * q.a * q.c) >= 0.f)
+// 	{
+// 		q.sq_diskr = sqrt(q.diskr);
+// 		q.t = (-q.b - q.sq_diskr) / q.a;
+// 		if (q.t > 0.f) {
+// 			hitpoint = vector_sum(vector_int_mult(dir, q.t), x);
+// 			len = scal_mult(hitpoint, parab->dir);
+// 			if (len < parab->angle && len > -zeroThreshold)
+// 				return (q.t);
+// 		}
+// 		q.t = (-q.b + q.sq_diskr) / q.a;
+// 		if (q.t > -zeroThreshold) {
+// 			hitpoint = vector_sum(vector_int_mult(dir, q.t), x);
+// 			len = scal_mult(hitpoint, parab->dir);
+// 			if (len < parab->angle && len > -zeroThreshold)
+// 				return (q.t);
+// 		}
+// 	}
+// 	return (0);
 
-}
+// }
 
 double ray_intersect_arrow(t_vector start, t_vector dir, t_obj *cyl)
 {
