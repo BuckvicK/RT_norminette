6b4c316946ab48df073914bc565c181a28a86a10
Artem <artem@Artems-iMac.local>
2019-08-09 14:46:06 +0300

    Remove useles function

--- edit.c
+++ edit.c
@@ -12,40 +12,43 @@
 
 #include "rtv1.h"
 
-static int	mouse_pressed1(int button, int x, int y,
-				t_rtv1 *rtv1, t_libui_button *buttons, int c_buttons)
-{
-	int		pressed_button;
-
-	if (y < 0)
-		return (0);
-	if (button == SDL_BUTTON_LEFT)
-	{
-		pressed_button = libui_isbuttonpressed(x, y, buttons, c_buttons);
-		if (pressed_button == -1)
-			return (0);
-		buttons[pressed_button].is_pressed = 1;
-		if (!ft_strcmp(buttons[pressed_button].function, "Radius+"))
-			rtv1->selected->radius += 0.1;
-		else if (!ft_strcmp(buttons[pressed_button].function, "Radius-"))
-			rtv1->selected->radius -= 0.1;
-	}
-	return (0);
-}
-
-static int	mouse_release1(t_libui_button *buttons ,int c_buttons)
-{
-	int		i;
-
-	i = 0;
-	while (i < c_buttons)
-	{
-		(buttons)[i].is_pressed = 0;
-		i++;
-	}
-	return (0);
-}
-
+/*
+** May be this function not need in this programm..
+**
+** static int	mouse_pressed1(int button, int x, int y,
+** 				t_rtv1 *rtv1, t_libui_button *buttons, int c_buttons)
+** {
+** 	int		pressed_button;
+** 
+** 	if (y < 0)
+** 		return (0);
+** 	if (button == SDL_BUTTON_LEFT)
+** 	{
+** 		pressed_button = libui_isbuttonpressed(x, y, buttons, c_buttons);
+** 		if (pressed_button == -1)
+** 			return (0);
+** 		buttons[pressed_button].is_pressed = 1;
+** 		if (!ft_strcmp(buttons[pressed_button].function, "Radius+"))
+** 			rtv1->selected->radius += 0.1;
+** 		else if (!ft_strcmp(buttons[pressed_button].function, "Radius-"))
+** 			rtv1->selected->radius -= 0.1;
+** 	}
+** 	return (0);
+** }
+** 
+** static int	mouse_release1(t_libui_button *buttons ,int c_buttons)
+** {
+** 	int		i;
+** 
+** 	i = 0;
+** 	while (i < c_buttons)
+** 	{
+** 		(buttons)[i].is_pressed = 0;
+** 		i++;
+** 	}
+** 	return (0);
+** }
+*/
 void		destroy_edit(t_rtv1 *rtv1)
 {
 	int		i;
