9da4485b05e67bc641e726edb63a972e61462adf
Artem <artem@Artems-iMac.local>
2019-08-09 13:27:45 +0300

    Norminette scene_selector.c, need split into different files

--- scene_selector.c
+++ scene_selector.c
@@ -1,92 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scene_selector.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chorange <chorange@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/20 19:25:19 by cocummin          #+#    #+#             */
+/*   Updated: 2019/07/31 14:18:22 by chorange         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "rtv1.h"
 
-void destroy_buttons(t_rtv1 *rtv1)
+void	destroy_buttons(t_rtv1 *rtv1)
 {
-    int i;
+	int		i;
 
-    i = 0;
-    while (i < rtv1->c_selector_buttons)
-    {
-        SDL_FreeSurface(rtv1->selector_buttons[i].pressed);
-        SDL_FreeSurface(rtv1->selector_buttons[i].surface);
-        i++;
-    }
-    rtv1->c_selector_buttons = 0;
+	i = 0;
+	while (i < rtv1->c_selector_buttons)
+	{
+		SDL_FreeSurface(rtv1->selector_buttons[i].pressed);
+		SDL_FreeSurface(rtv1->selector_buttons[i].surface);
+		i++;
+	}
+	rtv1->c_selector_buttons = 0;
 }
 
-void refresh_selector_buttons(t_rtv1 *rtv1)
+void	refresh_selector_buttons_1(t_rtv1 *rtv1, int i, char *str)
 {
-    int i;
-    char str[64];
-    char *tmp;
+	int		id;
+	char	*tmp;
 
-    
-
-    i = 0;
-    SDL_FillRect(rtv1->selector_surface, &((SDL_Rect){0, 0, 300, 1000}), 0x00FFFF00);
-    destroy_buttons(rtv1);
-    while (i < rtv1->scene.c_objs)
-    {
-        int id = 9999;
-        tmp = ft_itoa(rtv1->scene.objs[i].id);
-       // free(tmp);
-        ft_strcpy(str, tmp);
-        ft_strcat(str, " object");
-        if (rtv1->scene.objs[i].type == sphere)
-            ft_strcat(str, " sph");
-        else if (rtv1->scene.objs[i].type == cone)
-            ft_strcat(str, " cone");
-        else if (rtv1->scene.objs[i].type == cylinder)
-            ft_strcat(str, " cyl");
-        else if (rtv1->scene.objs[i].type == plane)
-            ft_strcat(str, " plane");
-        else if (rtv1->scene.objs[i].type == triangle)
-            ft_strcat(str, " poly");
-        
-        if (id == 9999 && rtv1->scene.objs[i].type != triangle)     
-            libui_newbutton((t_but_constr){20, 20 + 35*i, str, str, 0x0000ff55}, rtv1->selector_buttons, &(rtv1->c_selector_buttons));
-        if (rtv1->scene.objs[i].type == triangle)
-            id = rtv1->scene.objs[i].id;
-        free(tmp);
-        i++;
-    }
-    i = 0;
-    while (i < rtv1->scene.c_lights)
-    {
-        tmp = ft_itoa(rtv1->scene.lights[i].id);
-       // free(tmp);
-       // ft_strcpy(str, tmp);
-        ft_strcpy(str, tmp);
-        ft_strcat(str, " light");
-        if (rtv1->scene.lights[i].type == point)
-            ft_strcat(str, " point");
-        else if (rtv1->scene.lights[i].type == directional)
-            ft_strcat(str, " dir");
-        else if (rtv1->scene.lights[i].type == ambient)
-            ft_strcat(str, " amb");
-        libui_newbutton((t_but_constr){150, 20 + 35*i, str, str, 0x0000ff55}, rtv1->selector_buttons, &(rtv1->c_selector_buttons));
-        free(tmp);
-        i++;
-    }
+	id = 9999;
+	tmp = ft_itoa(rtv1->scene.objs[i].id);
+	ft_strcpy(str, tmp);
+	ft_strcat(str, " object");
+	if (rtv1->scene.objs[i].type == sphere)
+		ft_strcat(str, " sph");
+	else if (rtv1->scene.objs[i].type == cone)
+		ft_strcat(str, " cone");
+	else if (rtv1->scene.objs[i].type == cylinder)
+		ft_strcat(str, " cyl");
+	else if (rtv1->scene.objs[i].type == plane)
+		ft_strcat(str, " plane");
+	else if (rtv1->scene.objs[i].type == triangle)
+		ft_strcat(str, " poly");
+	if (id == 9999 && rtv1->scene.objs[i].type != triangle)     
+		libui_newbutton((t_but_constr){20, 20 + 35 * i, str, str,
+			0x0000ff55}, rtv1->selector_buttons, &(rtv1->c_selector_buttons));
+	if (rtv1->scene.objs[i].type == triangle)
+		id = rtv1->scene.objs[i].id;
+	free(tmp);
 }
 
-void scene_selector(t_rtv1 *rtv1)
+void	refresh_selector_buttons_2(t_rtv1 *rtv1, int i, char *str)
 {
-    rtv1->selector_surface = SDL_CreateRGBSurface(0, 300, 1000, 32, 0, 0, 0, 0);
-    rtv1->selector_window = SDL_CreateWindow("Selector", 100, 200, 300, 1000, SDL_WINDOW_SHOWN);
-    rtv1->selector_renderer = SDL_CreateRenderer(rtv1->selector_window, -1, SDL_RENDERER_ACCELERATED);
-    
+	char	*tmp;
+
+	tmp = ft_itoa(rtv1->scene.lights[i].id);
+	ft_strcpy(str, tmp);
+	ft_strcat(str, " light");
+	if (rtv1->scene.lights[i].type == point)
+		ft_strcat(str, " point");
+	else if (rtv1->scene.lights[i].type == directional)
+		ft_strcat(str, " dir");
+	else if (rtv1->scene.lights[i].type == ambient)
+		ft_strcat(str, " amb");
+	libui_newbutton((t_but_constr){150, 20 + 35 * i, str, str, 0x0000ff55},
+		rtv1->selector_buttons, &(rtv1->c_selector_buttons));
+	free(tmp);
+}
+
+void	refresh_selector_buttons(t_rtv1 *rtv1)
+{
+	int		i;
+	char	str[64];
+	char	*tmp;
+
+	i = 0;
+	SDL_FillRect(rtv1->selector_surface, &((SDL_Rect){0, 0, 300, 1000}),
+		0x00FFFF00);
+	destroy_buttons(rtv1);
+	while (i < rtv1->scene.c_objs)
+	{
+		refresh_selector_buttons_1(rtv1, i, str);
+		i++;
+	}
+	i = 0;
+	while (i < rtv1->scene.c_lights)
+	{
+		refresh_selector_buttons_2(rtv1, i, str);
+		i++;
+	}
+}
+
+void	scene_selector(t_rtv1 *rtv1)
+{
+	rtv1->selector_surface = SDL_CreateRGBSurface(0, 300, 1000, 32,
+		0, 0, 0, 0);
+	rtv1->selector_window = SDL_CreateWindow("Selector", 100, 200,
+		300, 1000, SDL_WINDOW_SHOWN);
+	rtv1->selector_renderer = SDL_CreateRenderer(rtv1->selector_window,
+		-1, SDL_RENDERER_ACCELERATED);
 	rtv1->selector_window_active = rtv1->next_win_ID;
-    rtv1->next_win_ID++;
-
-    
-
-    refresh_selector_buttons(rtv1);
+	rtv1->next_win_ID++;
+	refresh_selector_buttons(rtv1);
 }
 
-void destroy_scene_selector(t_rtv1 *rtv1)
+void	destroy_scene_selector(t_rtv1 *rtv1)
 {
-    SDL_DestroyWindow(rtv1->selector_window);
+	SDL_DestroyWindow(rtv1->selector_window);
 	rtv1->selector_window_active = 0;
-    SDL_FreeSurface(rtv1->selector_surface);
+	SDL_FreeSurface(rtv1->selector_surface);
 }
