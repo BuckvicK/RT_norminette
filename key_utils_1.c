a7df1ce41fe275a382770f3a812f91d59974160e
Artem <artem@Artems-iMac.local>
2019-08-11 22:29:32 +0300

    norminette in progress key_utils.c

--- key_utils_1.c
+++ key_utils_1.c
@@ -12,141 +12,147 @@
 
 #include "rtv1.h"
 
-int		mouse_pressed_1()
+int		mouse_norm_1(t_rtv1 *rtv1, int x, int y, char *func)
 {
+	t_list_item_addr	ret;
+
+	ret = list_utils(rtv1, x, y);
+	if (ret.item == -1)
+	{
+		rtv1->active_edit = libui_iseditpressed(x - CH, y, rtv1->edits, rtv1->c_edits);
+		if (rtv1->active_edit == -1)
+		{
+			libui_disactiveall(rtv1->edits, rtv1->c_edits, rtv1->lists, rtv1->c_lists);
+			return (-1);
+		}
+	}
+	else
+	{
+		rtv1->active_edit = -1;
+		ft_strcpy(func, rtv1->lists[ret.list].items[ret.item].function);
+	}
 	return (0);
 }
 
-int			mouse_pressed(int button, int x, int y, t_rtv1 *rtv1)
+int		mouse_norm(int x, int y, t_rtv1 *rtv1, int pressed_button, char *func)
+{
+	t_list_item_addr	ret;
+
+	if (pressed_button == -1)
+	{
+		mouse_norm_1(rtv1, x, y, func);
+	}
+	else
+	{
+		rtv1->active_edit = -1;
+		ft_strcpy(func, rtv1->buttons[pressed_button].function);
+	}
+	return (0);
+}
+
+int		mouse_pressed_left(int x, int y, t_rtv1 *rtv1)
 {
 	int					pressed_button;
 	char				func[64];
 	t_list_item_addr	ret;
 	int					selected_id;
 
+	if (x < 1000)
+		select_object(rtv1, x, y, &(rtv1->selected));
+	else
+	{
+		pressed_button = libui_isbuttonpressed(x - CH, y, rtv1->buttons, rtv1->c_buttons);
+		if (mouse_norm(x, y, rtv1, pressed_button, func) == -1)
+			return (0);
+		if (!ft_strcmp(func, "New Sphere"))
+		{
+			new_sphere(rtv1);
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "New Cylinder"))
+		{
+			new_cylinder(rtv1);
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "New Cone"))
+		{
+			new_cone(rtv1);
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "New Plane"))
+		{
+			new_plane(rtv1);
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "New PointL"))
+		{
+			new_point(rtv1);
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "New PointD"))
+		{
+			new_directional(rtv1);
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "Delete Object"))
+		{
+			if (rtv1->selected_light && rtv1->scene.c_lights)
+			{
+				selected_id = get_light_index_by_id(&rtv1->scene, rtv1->selected_light->id);
+				rtv1->scene.lights[selected_id] = rtv1->scene.lights[rtv1->scene.c_lights - 1];
+				rtv1->scene.c_lights--;
+				rtv1->scene.arrows_on = 0;
+				refresh_selector_buttons(rtv1);
+				return (0);
+			}
+			if (!rtv1->scene.c_objs)
+				return (0);
+			selected_id = (rtv1->selected - &rtv1->scene.objs[0]);
+			rtv1->scene.objs[selected_id] = rtv1->scene.objs[rtv1->scene.c_objs - 1];
+			rtv1->scene.c_objs--;
+			rtv1->scene.arrows_on = 0;
+			refresh_selector_buttons(rtv1);
+		}
+		else if (!ft_strcmp(func, "Save Scene"))
+			save(rtv1, rtv1->scene_file_name);
+		else if (!ft_strcmp(func, "Save As"))
+			save_as(rtv1);
+		else if (!ft_strcmp(func, "Ambient"))
+			rtv1->scene.advanced = (rtv1->scene.advanced ? 0 : 1);
+		else if (!ft_strcmp(func, "Soft Shadows"))
+			rtv1->scene.soft = (rtv1->scene.soft ? 0 : 1);
+		else if (!ft_strcmp(func, "Export BMP"))
+			IMG_SavePNG(rtv1->surface, "screens/1.png");
+		else if (!ft_strcmp(rtv1->buttons[pressed_button].function, "Edit"))
+		{	
+			if (rtv1->edit_window_active == 0)
+				edit(rtv1);
+		}
+		else if (!ft_strcmp(rtv1->buttons[pressed_button].function, "Selector"))
+		{	
+			if (rtv1->selector_window_active == 0)
+				scene_selector(rtv1);
+		}
+		else if (!ft_strcmp(func, "lol"))
+		{
+			if (rtv1->lists[0].is_dropped == 1)
+				rtv1->lists[0].is_dropped = 0;
+			else
+				rtv1->lists[0].is_dropped = 1;
+		}
+	}
+	rtv1->left_mouse_pressed = 1;
+	return (0);
+}
+
+int			mouse_pressed(int button, int x, int y, t_rtv1 *rtv1)
+{
 	rtv1->prev_x = x;
 	rtv1->prev_y = y;
 	if (y < 0)
 		return (0);
 	if (button == SDL_BUTTON_LEFT)
-	{
-		if (x < 1000)
-			select_object(rtv1, x, y, &(rtv1->selected));
-		else
-		{
-			pressed_button = libui_isbuttonpressed(x - CH, y, rtv1->buttons, rtv1->c_buttons);
-			if (pressed_button == -1)
-			{
-				ret = list_utils(rtv1, x, y);
-				if (ret.item == -1)
-				{
-					rtv1->active_edit = libui_iseditpressed(x - CH, y, rtv1->edits, rtv1->c_edits);
-					if (rtv1->active_edit == -1)
-					{
-						libui_disactiveall(rtv1->edits, rtv1->c_edits, rtv1->lists, rtv1->c_lists);
-						return (0);
-					}
-				}
-				else
-				{
-					rtv1->active_edit = -1;
-					ft_strcpy(func, rtv1->lists[ret.list].items[ret.item].function);
-				}
-			}
-			else
-			{
-				rtv1->active_edit = -1;
-				ft_strcpy(func, rtv1->buttons[pressed_button].function);
-			}
-			if (!ft_strcmp(func, "New Sphere"))
-			{
-				new_sphere(rtv1);
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "New Cylinder"))
-			{
-				new_cylinder(rtv1);
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "New Cone"))
-			{
-				new_cone(rtv1);
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "New Plane"))
-			{
-				new_plane(rtv1);
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "New PointL"))
-			{
-				new_point(rtv1);
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "New PointD"))
-			{
-				new_directional(rtv1);
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "Delete Object"))
-			{
-				if (rtv1->selected_light && rtv1->scene.c_lights)
-				{
-					selected_id = get_light_index_by_id(&rtv1->scene, rtv1->selected_light->id);
-					rtv1->scene.lights[selected_id] = rtv1->scene.lights[rtv1->scene.c_lights - 1];
-					rtv1->scene.c_lights--;
-					rtv1->scene.arrows_on = 0;
-					refresh_selector_buttons(rtv1);
-					return (0);
-				}
-				if (!rtv1->scene.c_objs)
-					return (0);
-				selected_id = (rtv1->selected - &rtv1->scene.objs[0]);
-				rtv1->scene.objs[selected_id] = rtv1->scene.objs[rtv1->scene.c_objs - 1];
-				rtv1->scene.c_objs--;
-				rtv1->scene.arrows_on = 0;
-				refresh_selector_buttons(rtv1);
-			}
-			else if (!ft_strcmp(func, "Save Scene"))
-			{	
-				save(rtv1, rtv1->scene_file_name);
-			}
-			else if (!ft_strcmp(func, "Save As"))
-			{
-				save_as(rtv1);
-			}
-			else if (!ft_strcmp(func, "Ambient"))
-			{
-				rtv1->scene.advanced = (rtv1->scene.advanced ? 0 : 1);
-			}
-			else if (!ft_strcmp(func, "Soft Shadows"))
-			{
-				rtv1->scene.soft = (rtv1->scene.soft ? 0 : 1);
-			}
-			else if (!ft_strcmp(func, "Export BMP"))
-			{
-				IMG_SavePNG(rtv1->surface, "screens/1.png");
-			}
-			else if (!ft_strcmp(rtv1->buttons[pressed_button].function, "Edit"))
-			{	
-				if (rtv1->edit_window_active == 0)
-					edit(rtv1);
-			}
-			else if (!ft_strcmp(rtv1->buttons[pressed_button].function, "Selector"))
-			{	
-				if (rtv1->selector_window_active == 0)
-					scene_selector(rtv1);
-			}
-			else if (!ft_strcmp(func, "lol"))
-			{
-				if (rtv1->lists[0].is_dropped == 1)
-					rtv1->lists[0].is_dropped = 0;
-				else
-					rtv1->lists[0].is_dropped = 1;
-			}
-		}
-		rtv1->left_mouse_pressed = 1;
-	}
+		mouse_pressed_left(x, y, rtv1);
 	else if (button == SDL_BUTTON_RIGHT)
 		rtv1->right_mouse_pressed = 1;
 	else if (button == SDL_BUTTON_MIDDLE)
